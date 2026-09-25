217e8596306ea3e71c1e870fe161eac976c97cb7
tomeenutitus-ops <tomeenutitus@gmail.com>
2026-09-25 09:12:15 +0000

    Initial dynamic website

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("title").textContent = "AWS DevOps Project";
+
+document.getElementById("message").textContent =
+    "Dynamic website is working!";
+
+document.getElementById("updated").textContent =
+    new Date().toLocaleTimeString();
