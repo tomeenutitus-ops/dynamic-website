ee3f468a2f65fefb6a106a49814676fdf1039eeb
tomeenutitus-ops <tomeenutitus@gmail.com>
2026-09-25 15:04:14 +0000

    Connect website to Lambda API

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-document.getElementById("title").textContent = "AWS DevOps Project";
-
-document.getElementById("message").textContent =
-    "Dynamic website is working!";
-
-document.getElementById("updated").textContent =
-    new Date().toLocaleTimeString();
+kfetch("https://ydjds63620.execute-api.ap-south-1.amazonaws.com/content")
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById("title").textContent = data.title;
+        document.getElementById("message").textContent = data.description;
+        document.body.style.background = data.theme === "dark" ? "#111" : "#fff";
+        document.getElementById("updated").textContent =
+            new Date().toLocaleTimeString();
+    })
+    .catch(error => {
+        console.error("Error:", error);
+    });
